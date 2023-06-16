48b72f7f45df39dff1318f281ffa37f7b0641527
Mehrad Nayyeri <mehrad.nayyeri@zohomail.com>
2023-06-16 16:43:29 +0330

    Removed everything from ConfigScreenBuilder which made more sense to use inheritance for them instead

--- src/client/java/ir/mehradn/mehradconfig/gui/ConfigScreenBuilder.java
+++ src/client/java/ir/mehradn/mehradconfig/gui/ConfigScreenBuilder.java
@@ -4,14 +4,11 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.components.Button;
-import net.minecraft.client.gui.navigation.ScreenRectangle;
-import net.minecraft.network.chat.Component;
-import java.util.List;
 
 @Environment(EnvType.CLIENT)
 public class ConfigScreenBuilder {
     /**
-     * Provides a number based on the properties of the screen. Usually width or height of a widget.
+     * Provides a number based on the properties of the screen. The number is usually width or height of a widget.
      */
     @FunctionalInterface
     @Environment(EnvType.CLIENT)
@@ -20,57 +17,22 @@ public class ConfigScreenBuilder {
     }
 
     /**
-     * Provides a rectangle based on the properties of the screen. Usually bounds of a button.
-     */
-    @FunctionalInterface
-    @Environment(EnvType.CLIENT)
-    public interface BoundsProvider {
-        ScreenRectangle get(int width, int height, Font font);
-    }
-
-    /**
-     * Properties for creating a custom button.
-     *
-     * @param message the message/text of the button
-     * @param bounds  the bounds of the button
-     * @param onPress the action that should happen when the button is clicked
-     */
-    @Environment(EnvType.CLIENT)
-    public record CustomButton(
-        Component message,
-        BoundsProvider bounds,
-        Button.OnPress onPress
-    ) {
-        /**
-         * @return a button based on the given properties
-         */
-        public Button build(int width, int height, Font font) {
-            ScreenRectangle rect = this.bounds.get(width, height, font);
-            return Button.builder(this.message, this.onPress).bounds(rect.left(), rect.top(), rect.width(), rect.height()).build();
-        }
-    }
-
-    /**
      * All of the properties for creating a config screen.
      *
      * @param widgetWidth      width of the entry widgets
+     * @param buttonWidth      width of the save and cancel button
      * @param descriptionY     y of the description text
      * @param descriptionWidth width of the description text
-     * @param saveBounds       bounds of the save button
-     * @param cancelBounds     bounds of the cancel button
-     * @param onSave           the action that should happen when the save button is pressed, after the config has been updated
+     * @param onSave           the action that should happen when the save button is pressed
      * @param onCancel         the action that should happen when the cancel button is pressed
-     * @param customButtons    a list of custom buttons to add to the screen
      */
     @Environment(EnvType.CLIENT)
     public record ScreenProperties(
         NumberProvider widgetWidth,
+        NumberProvider buttonWidth,
         NumberProvider descriptionY,
         NumberProvider descriptionWidth,
-        BoundsProvider saveBounds,
-        BoundsProvider cancelBounds,
         Button.OnPress onSave,
-        Button.OnPress onCancel,
-        List<CustomButton> customButtons
+        Button.OnPress onCancel
     ) { }
 }
